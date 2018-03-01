706eb95dd5ea8039cc5e1bc3790ef7a0dc96a53b
sarapcoding <35731084+sarapcoding@users.noreply.github.com>
2018-03-01 11:37:29 +0100

    first script

---
+++ starting_up.sh
@@ -0,0 +1,15 @@
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install vim
+sudo apt-get install mysql-server
+sudo apt-get install mysql-workbench
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-source
+apt-cache search jdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk/bin
+javac -version
+sudo tar -xvf spring-tool-suite-3.9.2.RELEASE-e4.7.2-linux-gtk-x86_64.tar.gz
+
